147e0c6d159ffc9cfb1c1f6eab5dfb4d11983c75
frandi rianto <frandirianto@gmail.com>
2020-11-30 07:00:04 +0700

    added auth

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+
+Route::group([
+    'prefix' => 'auth'
+], function () {
+    
+    Route::post('/login', 'AuthController@login');
+    Route::post('/signup', 'AuthController@signup');
+  
+    Route::group([
+      'middleware' => 'auth:api'
+    ], function() {
+        Route::get('/logout', 'AuthController@logout');
+        Route::get('/user', 'AuthController@user');
+    });
 });
