46a0e72a78951e2f4a743a38e72ee3a66a88e625
frandirianto <frandirianto@gmail.com>
2020-12-06 12:05:23 +0700

    modify auth controller and provider

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,53 +4,58 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Carbon\Carbon;
 
 class AuthController extends Controller
 {
-    public function signup(Request $request){
-        // $request->validate([
-        //     'name' => 'required|string',
-        //     'username' => 'required|string',
-        //     'email' => 'required|string|email|unique:users',
-        //     'password' => 'required|string|confirmed'
-        // ]);
-      
+    public function register(Request $request){      
+        $request = $request->data;
+
         $user = new User([
-            'name' => $request->name,
+            'name' => $request->fullname,
             'username' => $request->username,
             'email' => $request->email,
             'password' => bcrypt($request->password)
         ]);
+
         $user->save();
+        
         return response()->json([
             'message' => 'Successfully created user!'
         ], 201);
     }
 
     public function login(Request $request){
-        $request->validate([
-            'email' => 'required|string|email',
-            'password' => 'required|string',
-            'remember_me' => 'boolean'
-        ]);
-        $credentials = request(['email', 'password']);
-        if(!Auth::attempt($credentials))
+        
+        $credentials = [
+            'email' => $request->data['email'],
+            'password' => $request->data['password'],
+        ];
+        
+        if(!Auth::attempt($credentials)){
             return response()->json([
                 'message' => 'Unauthorized'
             ], 401);
-        $user = $request->user();
-        $tokenResult = $user->createToken('Personal Access Token');
-        $token = $tokenResult->token;
-        if ($request->remember_me)
-            $token->expires_at = Carbon::now()->addWeeks(1);
-        $token->save();
-        return response()->json([
-            'access_token' => $tokenResult->accessToken,
-            'token_type' => 'Bearer',
-            'expires_at' => Carbon::parse(
-                $tokenResult->token->expires_at
-            )->toDateTimeString()
-        ]);
+        }
+        else if(Auth::attempt($credentials)){
+            $user = Auth::user();
+            
+            $tokenResult = $user->createToken('Personal Access Token');
+            $token = $tokenResult->token;
+
+            if ($request->remember_me)
+                $token->expires_at = Carbon::now()->addWeeks(1);
+            $token->save();
+
+            return response()->json([
+                'access_token' => $tokenResult->accessToken,
+                'token_type' => 'Bearer',
+                'expires_at' => Carbon::parse(
+                    $tokenResult->token->expires_at
+                )->toDateTimeString()
+            ]);
+        }
     }
   
     public function logout(Request $request){
--- routes/api.php
+++ routes/api.php
@@ -18,11 +18,11 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::group([
-    'prefix' => 'auth'
+    'prefix' => 'web'
 ], function () {
     
     Route::post('/login', 'AuthController@login');
-    Route::post('/signup', 'AuthController@signup');
+    Route::post('/register', 'AuthController@register');
   
     Route::group([
       'middleware' => 'auth:api'
